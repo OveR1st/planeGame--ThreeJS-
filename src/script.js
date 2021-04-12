c567900917e35f9c2df8cd8b3e3fd716f354bb84
OveR1st <315overdose@gmail.com>
2021-04-12 21:25:14 +0300

    Pilot and him Hairs

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 import './style.scss'
 import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
-import {color} from "dat.gui";
 
 /**
  * Base
@@ -41,7 +40,8 @@ const colors = {
   brownDark: 0x23190f,
   wheat: 0xf7d9aa,
   red: 0xf25346,
-  white: 0xffffff
+  white: 0xffffff,
+  pink: 0xf5986e
 
 }
 const scene = new THREE.Scene();
@@ -74,12 +74,12 @@ const nearPlane = 1;
 const farPlane = 10000;
 
 const camera = new THREE.PerspectiveCamera(
-  fieldOfView,
-  aspectRatio,
-  nearPlane,
-  farPlane
+    fieldOfView,
+    aspectRatio,
+    nearPlane,
+    farPlane
 );
-camera.position.set(0, 100, 200);
+camera.position.set(0, 100, 70);
 
 // Controls
 // const controls = new OrbitControls(camera, canvas)
@@ -214,6 +214,87 @@ sky.mesh.position.y = -600;
 scene.add(sky.mesh);
 
 /**
+ * Pilot
+ */
+class Pilot {
+  constructor() {
+    this.mesh = new THREE.Object3D();
+    /**
+     * Body
+     */
+    const bodyGeom = new THREE.BoxGeometry(15, 15, 15);
+    const bodyMat = new THREE.MeshPhongMaterial({
+      color: colors.brown,
+      flatShading: true
+    })
+    const body = new THREE.Mesh(bodyGeom, bodyMat);
+    body.position.set(-2, -12, 0)
+    this.mesh.add(body)
+
+    /**
+     * Face
+     */
+
+    const faceGeom = new THREE.BoxGeometry(10, 10, 10);
+    const faceMat = new THREE.MeshLambertMaterial({
+      color: colors.pink
+    })
+
+    const face = new THREE.Mesh(faceGeom, faceMat)
+    this.mesh.add(face)
+
+    /**
+     * Hairs - Top
+     */
+    const hairGeom = new THREE.BoxGeometry(4, 4, 4);
+    const hairMat = new THREE.MeshLambertMaterial({
+      color: colors.brown
+    })
+
+    const hair = new THREE.Mesh(hairGeom, hairMat);
+    hair.geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(0, 2, 0));
+
+    const hairs = new THREE.Object3D();
+    this.hairsTop = new THREE.Object3D();
+
+    for (let i = 0; i < 12; i++) {
+      const h = hair.clone();
+      const col = i % 3;
+      const row = Math.floor(i / 3)
+      // console.log(Math.floor(i/3))
+      const startPosZ = -4;
+      const startPosX = -4;
+      h.position.set(startPosX + row * 4, 0, startPosZ + col * 4);
+      this.hairsTop.add(h)
+    }
+    hairs.add(this.hairsTop);
+    hairs.position.set(-5, 5, 0)
+
+    this.mesh.add(hairs)
+
+    /**
+     * Hairs - Side
+     */
+    const hairSideGeom = new THREE.BoxGeometry(12,4,2);
+    hairSideGeom.applyMatrix4(new THREE.Matrix4().makeTranslation(-6,0,0));
+    const hairSideR = new THREE.Mesh(hairSideGeom,hairMat);
+    const hairSideL = hairSideR.clone();
+    hairSideR.position.set(8, -2, 6);
+    hairSideL.position.set(8, -2, -6);
+    hairs.add(hairSideR)
+    hairs.add(hairSideL)
+
+    /**
+     * Hairs - Back
+     */
+    const hairBackGeom = new THREE.BoxGeometry(2,8,10);
+    const hairBack = new THREE.Mesh(hairBackGeom, hairMat);
+    hairBack.position.set(-1,-4,0);
+    hairs.add(hairBack)
+  }
+}
+
+/**
  * Plane
  */
 
@@ -313,7 +394,7 @@ class AirPlane {
      * Propeller
      */
 
-      // body
+        // body
     const geomPropeller = new THREE.BoxGeometry(20, 10, 10);
     const matPropeller = new THREE.MeshPhongMaterial({
       color: colors.brown,
@@ -331,7 +412,7 @@ class AirPlane {
       flatShading: true
     })
     const blade1 = new THREE.Mesh(geomBlade, matBlade);
-    blade1.position.set(8,0,0);
+    blade1.position.set(8, 0, 0);
     blade1.castShadow = true;
     blade1.receiveShadow = true;
     this.propeller.add(blade1);
@@ -347,26 +428,26 @@ class AirPlane {
      * Wheels
      */
 
-    const wheelProtecGeom = new THREE.BoxGeometry(30,15,10);
+    const wheelProtecGeom = new THREE.BoxGeometry(30, 15, 10);
     const wheelProtectMat = new THREE.MeshPhongMaterial({
       color: colors.red,
       flatShading: true
     })
     const wheelProtecR = new THREE.Mesh(wheelProtecGeom, wheelProtectMat);
-    wheelProtecR.position.set(25,-20,25);
+    wheelProtecR.position.set(25, -20, 25);
     this.mesh.add(wheelProtecR)
 
     const wheelProtecL = wheelProtecR.clone();
     wheelProtecL.position.z = -wheelProtecR.position.z;
     this.mesh.add(wheelProtecL);
 
-    const wheelTireGeom = new THREE.BoxGeometry(24,24,4);
+    const wheelTireGeom = new THREE.BoxGeometry(24, 24, 4);
     const wheelTireMat = new THREE.MeshPhongMaterial({
       color: colors.brownDark,
       flatShading: true
     })
     const wheelTireR = new THREE.Mesh(wheelTireGeom, wheelTireMat);
-    wheelTireR.position.set(25,-28,25)
+    wheelTireR.position.set(25, -28, 25)
     this.mesh.add(wheelTireR)
 
     const wheelTireL = wheelTireR.clone();
@@ -374,11 +455,11 @@ class AirPlane {
     this.mesh.add(wheelTireL)
 
     const wheelTireB = wheelTireR.clone();
-    wheelTireB.scale.set(.5,.5,.5)
-    wheelTireB.position.set(-35,-5,0)
+    wheelTireB.scale.set(.5, .5, .5)
+    wheelTireB.position.set(-35, -5, 0)
     this.mesh.add(wheelTireB);
 
-    const wheelAxisGeom = new THREE.BoxGeometry(10,10,6);
+    const wheelAxisGeom = new THREE.BoxGeometry(10, 10, 6);
     const wheelAxisMat = new THREE.MeshPhongMaterial({
       color: colors.brown,
       flatShading: true
@@ -387,17 +468,21 @@ class AirPlane {
     wheelTireR.add(wheelAxis)
 
 
-    const suspensionGeom = new  THREE.BoxGeometry(4,10,4)
-    suspensionGeom.applyMatrix(new THREE.Matrix4().makeTranslation(0,10,0))
+    const suspensionGeom = new THREE.BoxGeometry(4, 10, 4)
+    suspensionGeom.applyMatrix(new THREE.Matrix4().makeTranslation(0, 10, 0))
     const suspensionMat = new THREE.MeshPhongMaterial({
       color: colors.red,
       flatShading: true
     })
 
     const suspension = new THREE.Mesh(suspensionGeom, suspensionMat)
-    suspension.position.set(-35,-5,0);
+    suspension.position.set(-35, -5, 0);
     suspension.position.z = -.3;
     this.mesh.add(suspension);
+
+    this.pilot = new Pilot();
+    this.pilot.mesh.position.set(-10, 27, 0)
+    this.mesh.add(this.pilot.mesh)
 
     this.mesh.castShadow = true;
     this.mesh.receiveShadow = true;
@@ -420,6 +505,7 @@ const tick = () => {
 
   // mesh airplane
   airplane.mesh.rotation.y += .01;
+  airplane.mesh.rotation.z += .01;
   airplane.propeller.rotation.x += .3;
 
   // mesh sea
